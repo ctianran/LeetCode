0998f3b7153ae389686d8f37fc4bcd01bfdf1b6b
Tianran Chen <tristan20177@gmail.com>
2017-02-10 23:57:38 -0800

    add files

--- src/com/chentianran/Main.java
+++ src/com/chentianran/Main.java
@@ -1,8 +1,18 @@
 package com.chentianran;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("hello!");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        PriorityQueue<Integer> heap  = new PriorityQueue<Integer>(list);
+        System.out.println(heap.poll());
+        System.out.println(heap.poll());
     }
 }
